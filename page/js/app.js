17bd3af816c4aeaba07fca64b18ce62c6c213993
Nagata <kevin06092005@gmail.com>
2022-11-18 21:11:30 -0500

    api conectada, solucionar error de la navbar

---
+++ page/js/app.js
@@ -0,0 +1,24 @@
+const marvel = {
+  render: () => {
+    const urlAPI = 'https://gateway.marvel.com:443/v1/public/characters?ts=1000&apikey=ef320000ab2d8c3476184077179f34f6&hash=be856270c38db07f59b0fa9427d027a8';
+    const container = document.querySelector('#marvel-row');
+    let contentHTML = '';
+
+    fetch(urlAPI)
+      .then(res => res.json())
+      .then((json) => {
+        for (const hero of json.data.results) {
+          let urlHero = hero.urls[0].url;
+          contentHTML += `
+            <div class="col-md-4">
+                <a href="${urlHero}" target="_blank">
+                  <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}" class="img-thumbnail">
+                </a>
+                <h3 class="title">${hero.name}</h3>
+            </div>`;
+        }
+        container.innerHTML = contentHTML;
+      })
+  }
+};
+marvel.render();
